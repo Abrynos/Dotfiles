50db108e10afd212f9d61e5406ae8eab9dcb52b6
Abrynos <der.penner@gmx.at>
2019-01-12 15:43:32 +0100

    Fix multi-monitor  setups

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -5,7 +5,12 @@ TMPBG=/tmp/screen.png
 
 scrot $TMPBG
 convert $TMPBG -scale 10% -scale 1000% $TMPBG
-convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
-i3lock -t -i $TMPBG
-rm /tmp/screen.png
+if (( `xrandr | grep -c ' connected'` == 1 ));
+then
+	convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
+	i3lock -t -i $TMPBG
+else
+	i3lock -t -u -i $TMPBG
+fi
+rm $TMPBG
 
